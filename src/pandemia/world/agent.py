21b75219225d61054c8ccc23e2f06dede0d2e075
James-Thompson-724 <66617085+James-Thompson-724@users.noreply.github.com>
2022-11-11 12:36:12 +0000

    class documentation
    class documentation for Agent, Location and Region

--- src/pandemia/world/agent.py
+++ src/pandemia/world/agent.py
@@ -1,5 +1,4 @@
-"""Represents an individual, assigning to each an age, weekly routine and for each activity a list
-of locations at which they can perform that activity and corresponding weights"""
+"""Represents a individual"""
 
 import logging
 import uuid
@@ -9,9 +8,39 @@ from pandemia.world.location import Location
 log = logging.getLogger("agent")
 
 class Agent:
-    """Represents an individual"""
 
     def __init__(self, age):
+        """Represents a location, for example an area of land or a building.
+
+        Attributes:
+          uuid (UUID):
+            A universally unique identifier for this agent.
+          age (int):
+            The age of the individual, in years.
+          weekly_routine (list[str]):
+            A list of activites, representing a weekly routine. This specifies which activities this
+            agent performs and in which order. For example, suppose this agent performs two
+            activities, "Home" and "Work". Then their weekly routine might be:
+
+                ["Home", "Home", "Home", "Home", "Work", "Home", "Home", "Work", "Home",
+                 "Home", "Work", "Home", "Home", "Work", "Home", "Home", "Work", "Home",
+                 "Home", "Home", "Home"]
+
+            In this example, it is to be understood that each string refers to an eight-hour time
+            interval, since it is to be assumed that the week is divided into time intervals of
+            equal length.
+          activity_locations (dict[str, list[Location]]):
+            For each string appearing in their weekly routine, a list of objects of type Location
+            is assigned, indicating at which locations it is posisble for the agent to perform that
+            activity. In the above example, an agent would require two lists, one for activity
+            "Home" and one for activity "Work". The list for activity "Home" might consist of a
+            single Location of an appropriate type, for example "House".
+          activity_location_weights (dict[str, list[float]]):
+            For each of the above lists, an associated list of weights, indicating how likely it is
+            that the agent will choose the corresponding locations when performing each activity.
+            These lists of weights should therefore be the same length as the lists of locations,
+            for each activity. The weights need not sum to 1.
+        """
 
         self.uuid = uuid.uuid4().hex
         self.age: int = age
