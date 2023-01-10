0a42b83700b29dfa19a5bb565b12e0f90aa66ac8
James-Thompson-724 <66617085+James-Thompson-724@users.noreply.github.com>
2023-01-10 15:24:54 +0000

    Update simulator.py

--- src/pandemia/simulator.py
+++ src/pandemia/simulator.py
@@ -341,23 +341,22 @@ class Simulator:
             ("pygame_shapes.PygameShapes" in self.config['reporters']):
 
             infections =\
-                np.zeros((self.number_of_regions * self.number_of_strains), dtype=np.uint64)
+                np.zeros((self.number_of_regions * self.number_of_strains), dtype=np.int64)
 
-
-            # for vector_region in self.vector_regions:
-            #     self.collect_telemetry_data(
-            #         c_int(vector_region.number_of_agents),
-            #         c_int(self.number_of_strains),
-            #         c_int(vector_region.id),
-            #         c_void_p(vector_region.current_strain.ctypes.data),
-            #         c_void_p(infections.ctypes.data)
-            #     )
             for vector_region in self.vector_regions:
-                for n in range(vector_region.number_of_agents):
-                    if vector_region.current_strain[n] != -1:
-                        infections[(vector_region.id * self.number_of_strains) + vector_region.current_strain[n]] += 1
+                self.collect_telemetry_data(
+                    c_int(vector_region.number_of_agents),
+                    c_int(self.number_of_strains),
+                    c_int(vector_region.id),
+                    c_void_p(vector_region.current_strain.ctypes.data),
+                    c_void_p(infections.ctypes.data)
+                )
+            # for vector_region in self.vector_regions:
+            #     for n in range(vector_region.number_of_agents):
+            #         if vector_region.current_strain[n] != -1:
+            #             infections[(vector_region.id * self.number_of_strains) + vector_region.current_strain[n]] += 1
 
-            infections = ((1 / self.vector_world.scale_factor) * infections).astype(np.uint64)
+            infections = ((1 / self.vector_world.scale_factor) * infections).astype(np.int64)
             self.telemetry_bus.publish("strain_counts.update", self.clock, infections)
 
         if ("plot.PlotDeaths" in self.config['reporters']):
